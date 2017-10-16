a2232408fdf0946d3089bfd24f032ef994ffc492
ShilinChen1101 <31868277+ShilinChen1101@users.noreply.github.com>
2017-10-16 16:16:13 -0400

    d3.2

---
+++ numbers.js
@@ -0,0 +1,17 @@
+    function plusTen(num){
+      return num + 10;
+    }
+
+
+    plusTen(20);
+
+    function minusTen(num){
+      return num - 10;
+    }
+minusTen(100);
+
+
+    var numbers = {
+    plus10: plusTen,
+    minus10: minusTen
+    };
